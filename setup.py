effb6b5aae71d6911ae5306c2d46e4c2df90d71b
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-02-09 19:21:57 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -24,12 +24,7 @@ setup(
             'GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Topic :: Home Automation',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
