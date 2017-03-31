5ba19d3a4487c4d2ffd2081635ae54d0eb411d33
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-03-31 02:41:50 -0400

    Added Python 3.6

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Home Automation',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
