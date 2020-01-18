4512c40278920855f1195335e3865c53eaf17da3
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2020-01-18 15:46:21 -0500

    Removed python 3.5

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setup(
         'GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Home Automation',
