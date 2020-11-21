c3e98c9ce3da24f54eb4c38664356f644b7c8a47
Paulus Schoutsen <paulus@home-assistant.io>
2020-11-21 22:12:05 +0100

    Unpin reqs #222

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     license='LGPLv3+',
     include_package_data=True,
     install_requires=[
-        'requests==2.22.0',
-        'requests-oauthlib==1.3.0',
-        'oauthlib==3.1.0',
+        "requests>=2.0.0",
+        "requests-oauthlib>=1.3.0,<2",
+        "oauthlib>=3.0.0,<4",
         'pytz'
     ],
     test_suite='tests',
